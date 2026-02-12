dc781ae2930e32463bc91f7f5f3f6db69d05b3dc
Lewis Rey <lewisu99@gmail.com>
2026-02-12 22:14:14 +0800

    Instructor Part Merge

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,6 @@
-import { StrictMode } from 'react'
+import { StrictMode } from 'react' 
 import { createRoot } from 'react-dom/client'
-<<<<<<< HEAD
-=======
-import './index.css'
->>>>>>> 81e3fbeddc448d911510f20108f65f88cd77d537
+
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-<<<<<<< HEAD
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
 })
-=======
-import tailwindcss from '@tailwindcss/vite'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(), 
-  ],
-})
->>>>>>> 81e3fbeddc448d911510f20108f65f88cd77d537
